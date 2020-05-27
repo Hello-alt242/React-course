ade01a951d02d76aec590d71699a42b214a3afc0
Hello-alt242 <65467104+Hello-alt242@users.noreply.github.com>
2020-05-27 15:30:49 -0400

    Now it is correct for JSX

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const App=function(){
     return <div>
         <label class="label" for="name">Enter name:</label>
         <input id="name" type="text"/>
-        <button style="background-color:blue; color: white;">Submit</button>
+        <button style={{ backgroundColor: 'blue', color: 'white'}}>Submit</button>        
   </div>
 };
 
