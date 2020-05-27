daa0b31bff296a1d07bbd7701becc05bc0fe9cf8
Hello-alt242 <65467104+Hello-alt242@users.noreply.github.com>
2020-05-27 15:27:02 -0400

    This is the HTML version, will throw an error

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import ReactDOM from 'react-dom';
 
 //Create a react component
 const App=function(){
-    return <div>Hi there!</div>;
+    return <div>
+        <label class="label" for="name">Enter name:</label>
+        <input id="name" type="text"/>
+        <button style="background-color:blue; color: white;">Submit</button>
+  </div>
 };
 
 
