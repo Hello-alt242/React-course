07ab03034fe4bd708a541a52d305e72d93213ffc
Hello-alt242 <65467104+Hello-alt242@users.noreply.github.com>
2020-06-01 19:19:42 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+//import react and react-dom libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Create a react component
+const App=function(){
+    const buttonText={text: 'Click me'};
+
+    return <div>
+        <label className="label" htmlFor="name">
+            Enter name:
+        </label>
+        <input id="name" type="text"/>
+        <button style={{ backgroundColor: 'blue', color: 'white'}}>
+            {buttonText.text}
+        </button>        
+  </div>
+};
+
+
+// Take the react component and show it on screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
